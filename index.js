9a18d64e66e5f7e795820e8c7ab270e9819f3e77
shouriguptar <shourigupta.rv@citrix.com>
2019-01-24 11:36:36 +0530

    no message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
-server.connection({ port: 3000, host: 'localhost' });
+server.connection({ port: 8080, host: 'localhost' });
 
 server.route({
     method: 'GET',
