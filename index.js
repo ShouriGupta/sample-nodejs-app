65a8044568f90cf76affd968bf2645ef0d810715
shouriguptar <shourigupta.rv@citrix.com>
2019-01-24 11:30:34 +0530

    Simple hapi server

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
-var http = require('http');
-var KeyVault = require('azure-keyvault');
-var AuthenticationContext = require('adal-node').AuthenticationContext;
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
 
-var server = http.createServer(function (request, response) {
-
-    var clientId = "f3dfef1b-e097-4e19-91f6-ee6274875523";
-    var clientSecret = "nj8XoXkSVcITV5ldlCbwSZ9Mr0F9JL5+Ebc2dDO5rQA=";
-    var vaultUri = "https://unit-test-key-vault.vault.azure.net";
-
-    var authenticator = function (challenge, callback) {
-        // Create a new authentication context.
-        var context = new AuthenticationContext(challenge.authorization);
-
-        // Use the context to acquire an authentication token.
-        return context.acquireTokenWithClientCredentials(challenge.resource, clientId, clientSecret, function (err, tokenResponse) {
-            if (err) throw err;
-            // Calculate the value to be set in the request's Authorization header and resume the call.
-            var authorizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
-
-            return callback(null, authorizationValue);
-        });
-
-    };
-
-    var credentials = new KeyVault.KeyVaultCredentials(authenticator);
-    var client = new KeyVault.KeyVaultClient(credentials);
-
-    console.log(client)
-
-    client.createKey(vaultUri, 'yourkey', 'EC', function (err, keyBundle) {
-        if (err) {
-            console.log("The err : ", err);
-            response.writeHead(200, { "Content-Type": "text/plain" });
-            response.end("Hello World! No" + err);
-        }
-        else {
-            console.log("The key : ", keyBundle);
-            response.writeHead(200, { "Content-Type": "text/plain" });
-            response.end("Hello World! Yes" + keyBundle);
-        }
-    });
-
-
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+    	reply("Hello World")
+    }
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
